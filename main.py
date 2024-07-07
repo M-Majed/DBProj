207f75c0e146e99767d869ded2dbbadf6cbf1a1d
M-Majed <m.majed@ec.iut.ac.ir>
2024-07-07 17:23:01 +0330

    feat: update music browsing, database connection handling, and user registration

--- main.py
+++
@@ -1,6 +0,0 @@
-from PyQt5 import QtCore, QtGui, QtWidgets
-import resources
-#* to open another window
-from login_signup import UI_LoginSignupWindow
-from login import Ui_LoginWindow 
-from Signup import Ui_SignupWindow
